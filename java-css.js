817cfcc60f94efde4cdf5825748f49ee8b16e9da
Jorge <jorgejunior1970@hotmail.com>
2019-08-13 19:42:36 -0500

    Version 1 Botones Funcionales

---
+++ java-css.js
@@ -0,0 +1,421 @@
+//ESTILO TABLERO
+$('.Negro').css({'background-color': 'black', 'color' : 'white'})
+$('.Negro').width('100px');
+$('.Negro').height('100px');
+$('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+$('.Blanco').width('100px');
+$('.Blanco').height('100px');
+
+//ESTILO BOTONES
+$('h1').css({'margin-bottom': '50px','align-content' : 'center'});
+$('button').css({'margin': '5px', 'background-color' : 'black'});
+
+//ESQUINAS
+$('button').eq(0).click(function(){
+    $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+$('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Negro')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(31)
+    .css('background-color', 'red');
+    })
+
+    //Z
+    $('button').eq(1).click(function(){
+        $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+    $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Negro')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(1)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(1)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(2)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(2)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(7)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(10)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(14)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(21)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(24)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(29)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(29)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(30)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(30)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(31)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(31)
+    .css('background-color', 'red');
+    })
+ 
+    // X
+    $('button').eq(2).click(function(){
+        $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+    $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(7)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(10)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(14)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(21)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(24)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(4)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(9)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(13)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(18)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(22)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(27)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(31)
+    .css('background-color', 'red');
+    })
+    
+    // +
+    $('button').eq(3).click(function(){
+        $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+    $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Blanco')
+    .eq(1)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(2)
+    .css('background-color', 'red');   
+    $('.Blanco')
+    .eq(6)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(5)
+    .css('background-color', 'red');    
+    $('.Blanco')
+    .eq(9)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(10)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(18)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(13)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(14)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(12)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(12)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(15)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(14)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(13)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(15)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(16)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(16)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(18)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(19)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(19)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(22)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(21)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(25)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(26)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(30)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(29)
+    .css('background-color', 'red');
+    })
+
+    //DIAGONAL
+$('button').eq(4).click(function(){
+    $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+    $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(7)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(10)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(14)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(21)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(24)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(28)
+    .css('background-color', 'red');
+    })
+
+    //DIAGONAL INVERTIDA
+    $('button').eq(5).click(function(){
+        $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+        $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    
+    $('.Negro')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(4)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(9)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(13)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(18)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(22)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(27)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(31)
+    .css('background-color', 'red');
+    })
+
+    //CONTORNO
+    $('button').eq(6).click(function(){
+        $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+        $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+    $('.Negro')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(0)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(1)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(1)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(2)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(2)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(3)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(28)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(29)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(29)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(30)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(30)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(31)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(31)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(4)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(7)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(8)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(11)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(12)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(15)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(16)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(19)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(20)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(23)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(24)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(27)
+    .css('background-color', 'red');
+    })
+
+//CENTRO
+$('button').eq(7).click(function(){
+    $('.Negro').css({'background-color': 'black', 'color' : 'white'})
+    $('.Blanco').css({'background-color' : 'white' , 'color' : 'black'})
+   
+    $('.Negro')
+    .eq(18)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(17)
+    .css('background-color', 'red');
+    $('.Negro')
+    .eq(13)
+    .css('background-color', 'red');
+    $('.Blanco')
+    .eq(14)
+    .css('background-color', 'red');
+})
